8a870524259a65295b339b62c578bf099ff99d4a
1490340403 <1490340403@qq.com>
2020-08-01 18:10:49 +0800

    订单页面完成

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,30 +1,110 @@
 <template>
-  <div>11</div>
+  <div class="order">
+     <div class="address" v-if="address.name" @click="toAddressList">
+      <div class="item">
+        <div class="list">
+          <div class="addresslist">
+            <div>
+              <span>{{address.name}}</span>
+              <div class="moren">默认</div>
+            </div>
+            <div class="info">
+              <p>{{address.mobile}}</p>
+              <p>{{address.address+address.address_detail}}</p>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+     <div class="seladdress" v-else @click="toAdd">请选择默认地址</div>
+
+    <div class="orderbox">
+      <div class="item">
+        <div>商品合计</div>
+        <div>¥{{allprice}}</div>
+      </div>
+      <div class="item">
+        <div>运费</div>
+        <div>免运费</div>
+      </div>
+      <div class="item">
+        <div>优惠券</div>
+        <div>暂无</div>
+      </div>
+    </div>
+
+    <div class="cartlist">
+      <div class="item" v-for="(item, index) in listData" :key="index">
+        <div class="con">
+          <div class="left">
+            <div class="img">
+              <img :src="item.list_pic_url" alt="">
+            </div>
+            <div class="info">
+              <p>{{item.goods_name}}</p>
+              <p>¥{{item.retail_price}}</p>
+            </div>
+          </div>
+          <div class="right">
+            <div class="num">x{{item.number}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bottom">
+      <div>实付：¥ {{allprice}}</div>
+      <div class="pay" @click="pay">支付</div>
+    </div>
+  </div>
 </template>
 
 <script>
+import './index.css'
 import { request } from "../../utils/index";
 export default {
 data(){
     return {
         openId: "oQmbb4sNZdxaUQZ0sfYgvtOP2S7c",
-        addressId:''
+        addressId:'',
+        address: {},
+      price: '',
+      allprice: '',
+      addressId: '',
+      listData: []
     }
- },
- show(){
-     this.getOrders()
  },
  mounted(){
      this.getOrders()
  },
  methods:{
+   toAddressList(){
+     wx.navigateTo({
+       url: '/pages/selectAddress/main'
+     });
+       
+   },
+   pay(){
+     wx.showToast({
+      title: '该功能未上线！',
+      icon: 'none',
+      duration: 1500
+    })
+   },
     async getOrders(){
-        console.log(1)
         const data=await request('/getOrder','GET',{
             openId:this.openId,
             addressId:this.addressId
         })
-      console.log(data)
+           if (data) {
+        // this.allprice = data.price
+        this.listData = data.goodsList
+        this.address = data.address
+      }
+      this.listData.map((item) => {
+        this.allprice = Number(item.retail_price * item.number) + Number(this.allprice)
+      })
     }
   }
 }

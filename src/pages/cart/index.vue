82f3ca57a42486e47eeaad5440776eb025898aa8
1490340403 <1490340403@qq.com>
2020-08-03 23:28:13 +0800

    购物车功能

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -39,16 +39,109 @@
 
 <script>
 import './index.css'
+import { request } from "../../utils/index";
 export default {
 data(){
     return{
+        openId: "oQmbb4sNZdxaUQZ0sfYgvtOP2S7c",
         listData:[],
         Listids:[],
-        allPrice:0,
-        isCheckedNumber:0
+    
+    
+        allcheck:false
     }
+  },
+mounted(){
+  this.getData()
+},
+computed:{
+  
+  isCheckedNumber(){
+    let number = 0
+      for (let i = 0; i < this.Listids.length; i++) {
+        if (this.Listids[i]) {
+          number++
+        }
+      }
+      if (number == this.listData.length && number !== 0) {
+        this.allcheck = true
+      } else {
+        this.allcheck = false
+      }
+      return number
+  },
+      allPrice () {
+      let Price = 0
+      for (let i = 0; i < this.Listids.length; i++) {
+        if (this.Listids[i]) {
+          Price += this.listData[i].retail_price * this.listData[i].number
+        }
+      }
+      return Price
+    }
+},
+methods:{
+  async orderDown () {
+    console.log(this.Listids,this.Listids.length)
+      if (this.Listids.length === 0) {
+        wx.showToast({
+          title: '请选择商品',
+          icon: 'none',
+          duration: 1500
+        })
+        return false
+      }
+      // 去除数组中空的false
+      let newgoodsid = []
+      for (let i = 0; i < this.Listids.length; i++) {
+        const element = this.Listids[i]
+        if (element) {
+          newgoodsid.push(element)
+        }
+      }
+      let goodsId = newgoodsid.join(',')
+      const data = await request('/getOrder', 'GET',{
+        goodsId: goodsId,
+        openId: this.openId,
+        allPrice: this.allPrice
+      })
+      if (data) {
+        wx.navigateTo({
+          url: '/pages/order/main'
+        });
+      }
+    },
+  allCheck(){
+   this.Listids = []
+   console.log(22,this.allcheck)
+      if (this.allcheck) {
+        this.allcheck = false
+      } else {
+        this.allcheck = true
+        // 全部选择
+        for (let i = 0; i < this.listData.length; i++) {
+          const element = this.listData[i]
+          this.Listids.push(element.goods_id)
+        }
+      }
+  },
+  changeColor(index,id){
+    if (this.Listids[index]) {
+        this.$set(this.Listids, index, false)
+      } else {
+        this.$set(this.Listids, index, id)
+      }
+  },
+ async getData(){
+   const data=await request('/getCartData','GET',{
+     openId: this.openId,
+   })
+   this.listData=data.data
+   
+  }
 }
 }
+
 </script>
 
 <style>
